bddf42b01f60b9ac8dc8c81c333ce3d17b1ba255
cedricdre <cedric.dreue@gmail.com>
2023-12-06 13:22:47 +0100

    MaJ mon theme

---
+++ wp-content/themes/mon-theme/index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400;700&display=swap" rel="stylesheet">
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+   <link href="./assets/css/style.css" rel="stylesheet">
+   <title>Document</title>
+</head>
+
+<body>
+
+   <header>
+      <nav class="navbar navbar-expand-lg bg-danger p-O">
+         <div class="container">
+            <img src="/wp-content/themes/mon-theme/assets/img/logo-montheme.jpg" alt="Logo" class="img-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+               <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+               <ul class="navbar-nav">
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Catégorie 1</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Catégorie 2</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="#">Catégorie 3</a>
+                  </li>
+               </ul>
+            </div>
+         </div>
+      </nav>
+   </header>
+
+   <main>
+
+   </main>
+
+   <footer>
+
+   </footer>
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+
+</html>
